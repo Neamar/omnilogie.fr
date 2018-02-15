fcd5a4ebdc7ea4f8e1e33d355c721f8f6fc24791
Neamar <neamar@neamar.fr>
2018-02-15 23:16:30 +0000

    no md5

--- cron-tweet.php
+++ cron-tweet.php
@@ -1,5 +1,5 @@
 <?php
-if(md5($_GET['code'])==getenv("ACCESS_TOKEN_SECRET")) {
+if($_GET['code']==getenv("ACCESS_TOKEN_SECRET")) {
   External::tweet("Oh. Bonjour ? Ça faisait longtemps ;)");
   echo "DONE";
 } else {
