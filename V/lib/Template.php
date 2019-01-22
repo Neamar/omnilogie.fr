dc2701d33f067112d19d0dcc021b06c7e2f4cc3b
Neamar <neamar@neamar.fr>
2019-01-22 13:55:20 +0000

    Updated google analytics code

--- V/lib/Template.php
+++ V/lib/Template.php
@@ -11,18 +11,16 @@
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<link rel="canonical" href="https://omnilogie.fr<?php Template::put('CanonicalURL') ?>" />
-	<script type="text/javascript">
-	var inits=[];
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-4257957-3']);
-	_gaq.push(['_trackPageview']);
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-4257957-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
 
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+	  gtag('config', 'UA-4257957-3');
 	</script>
+
 	<script type="text/javascript" src="/images/script.js" async></script>
 	<script type="text/x-mathjax-config">
 	MathJax.Hub.Config({
