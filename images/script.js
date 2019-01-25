73e8828adeecfa6867ee5d3cdad9e19ec87ab2b3
Neamar <neamar@neamar.fr>
2019-01-25 18:19:52 +0000

    Allow clicks on banners

--- images/script.js
+++ images/script.js
@@ -1,3 +1,5 @@
+var inits = [];
+
 window.onload=function()
 {
 	for(var i = 0;i < inits.length;i++)
@@ -10,7 +12,7 @@ function bannerLink()
 	var Images = document.getElementsByTagName('img');
 	for(i=0;i<Images.length;i++)
 	{
-		if(Images[i].src.replace('http://'+document.location.host,'').substr(0,15) == '/images/Banner/')
+		if(Images[i].src.replace('https://'+document.location.host,'').substr(0,15) == '/images/Banner/')
 		{
 			Images[i].onclick=function(){document.location='/O/' + this.alt.replace(/ /g,'_')};
 			Images[i].style.cursor = 'pointer';
