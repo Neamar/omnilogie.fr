f00e9986e64f1e41813c6cd24576985cc4337239
Neamar <neamar@neamar.fr>
2019-02-09 22:30:39 +0000

    https for the feed

--- V/flux.php
+++ V/flux.php
@@ -23,11 +23,11 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 <rss version="2.0">
 <channel>
 	<title>Omnilogismes du jour : questions et réponses de culture générale</title>
-	<link>http://omnilogie.fr</link>
+	<link>https://omnilogie.fr</link>
 	<image>
 		<url>http://omnilogie.fr/images/Question.png</url>
 		<title>Omnilogismes du jour : questions et réponses de culture générale</title>
-		<link>http://omnilogie.fr</link>
+		<link>https://omnilogie.fr</link>
 	</image>
 	<description>Chaque jour, un article de culture générale sur tout et n'importe quoi. Une infusion de savoir quotidienne !</description>
 	<managingEditor>omni@neamar.fr (Administrateurs Omnilogie)</managingEditor>
@@ -39,7 +39,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 		<pubDate><?php echo date('r',$Article->Timestamp) ?></pubDate>
 		<author>omni@neamar.fr (<?php utf($Article->Auteur) ?>)</author>
 		<guid><?php echo URL . Link::omniShort($Article->ID) ?></guid>
-		<link>http://omnilogie.fr<?php echo str_replace('%2FO%2F','/O/',urlencode(Link::omni($Article->Titre))) ?></link>
+		<link>https://omnilogie.fr<?php echo str_replace('%2FO%2F','/O/',urlencode(Link::omni($Article->Titre))) ?></link>
 		<description>
 			<![CDATA[<?php utf(str_replace($C['Cherche'],$C['Remplace'],$Article->getBanner())); ?>
 
