79e231edbbb275f15cb7f682d22cdad63db78030
Neamar <neamar@neamar.fr>
2019-02-13 20:21:17 +0000

    Removed newrelic.ini
    Configuration can be defined from server side

--- index.php
+++ index.php
@@ -55,6 +55,10 @@ spl_autoload_register(function ($Class)
 if(empty($_GET['P']))
 	$_GET['P']='index';
 
+if (extension_loaded('newrelic')) { // Ensure PHP agent is available
+  newrelic_name_transaction($_GET['P']);
+}
+
 //Charger les constantes utiles au site.
 Constants::load();
 
