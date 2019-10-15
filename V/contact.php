7ca59bc52a055346ec609344e1937d49e5961ce9
Neamar <neamar@neamar.fr>
2019-10-15 16:33:43 +0200

    Updated contact form

--- V/contact.php
+++ V/contact.php
@@ -12,10 +12,11 @@
 
 <p>Besoin d'entrer en contact avec les administrateurs du site ? Utilisez ce formulaire pour signaler une erreur sur un article, une faute, un probl&egrave;me, un bug, une suggestion, un partenariat, votre admiration sans bornes, votre haine, ou tout ce qui vous passe par la t&ecirc;te vaguement en rapport avec Omnilogie.</p>
 
-<form method="post" action="https://neamar.fr/email.php" accept-charset="utf-8">
+<form method="post" action="https://neamar.fr/email.php" accept-charset="utf-8" onsubmit="document.getElementById('protection').value = 2">
 	<p><label for="mail">Votre e-mail :</label>
 	<input type="email" name="_replyto" id="mail" autofocus required/></p>
 	<input type="hidden" name="_to" value="neamar@neamar.fr" />
+	<input type="hidden" name="protection" id="protection" value="1" />
 	<p><label for="titre">Titre :</label>
 	<input type="text" name="_subject" id="titre" value="<?php echo (isset($_POST['titre'])?$_POST['titre']:'') ?>" /></p>
 
