e12d9ff3372aa904955af6dde6735bb7a3fb2ba2
Neamar <neamar@neamar.fr>
2024-09-30 12:52:22 +0200

    Braces

--- C/membres/oubli.php
+++ C/membres/oubli.php
@@ -19,16 +19,16 @@ if(isset($_POST['mail']))
 		$chars_length = (strlen($chars) - 1);
 
 		// Start our string
-		$string = $chars{rand(0, $chars_length)};
+		$string = $chars[rand(0, $chars_length)];
 
 		// Generate random string
 		for ($i = 1; $i < $length; $i = strlen($string))
 		{
 			// Grab a random character from our list
-			$r = $chars{rand(0, $chars_length)};
+			$r = $chars[rand(0, $chars_length)];
 
 			// Make sure the same two characters don't appear next to each other
-			if ($r != $string{$i - 1}) $string .=  $r;
+			if ($r != $string[$i - 1]) $string .=  $r;
 		}
 
 		// Return the string
