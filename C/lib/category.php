b917c2aa55446a94eb24b6d188cec0918aa2d980
Neamar <neamar@neamar.fr>
2024-09-30 14:12:02 +0200

    drop error_log

--- C/lib/category.php
+++ C/lib/category.php
@@ -59,7 +59,6 @@ class Category
 		self::buildInitialTree();
 
 		$KeyWords=array();
-		error_log(print_r($Categories, TRUE));
 		foreach($Categories as $Category)
 			self::getTreeR($Category,$KeyWords);
 		return $KeyWords;
